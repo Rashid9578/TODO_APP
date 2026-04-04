e8de564df53f504aeb74d9aad07a87d38879e0f1
Rashid Ahmed Siddiquee <rashidahmedsiddiquee607@gmail.com>
2026-04-05 03:10:22 +0530

    Initial commit - Todo App

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
+
+let port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'));
+
+
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`);
+});
+
+let arr = [
+    {
+        id: uuidv4(),
+        task: "DSA",
+        done: false
+    },
+    {
+        id: uuidv4(),
+        task: "Backend",
+        done: false
+    },
+    {
+        id: uuidv4(),
+        task: "LeetCode",
+        done: false
+    },
+    {
+        id: uuidv4(),
+        task: "CodeChef",
+        done: false
+    }
+];
+
+app.get("/tasks", (req, res) => {
+    res.render("index.ejs", { arr });
+});
+
+app.get("/tasks/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/tasks", (req, res) => {
+    let id = uuidv4();
+    let task = req.body.task;
+    arr.push({ id, task, done: false });
+    res.redirect("/tasks");
+});
+
+app.get("/tasks/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let work = arr.find((a) => id === a.id);
+    
+    res.render("edit.ejs", { work : work });
+});
+
+app.patch("/tasks/:id", (req, res) => {
+    let { id } = req.params;
+    let task = req.body.task;
+    let work = arr.find((a) => id === a.id);
+    work.task = task;
+    res.redirect("/tasks");
+});
+
+app.delete("/tasks/:id", (req, res) => {
+    let { id } = req.params;
+    let work = arr.find((a) => id === a.id);
+    let idx = arr.indexOf(work);
+    arr.splice(idx, 1);
+    res.redirect("/tasks");
+});
+
+app.patch("/tasks/:id/toggle", (req, res) => {
+    let { id } = req.params;
+
+    let work = arr.find((a) => id === a.id);
+    work.done = !work.done;
+
+    res.redirect("/tasks");
+});
